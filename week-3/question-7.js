671086543c44d1246eed8994c3dd2e2259eff85f
HossainRezayi <hossainrezayi26.@gmail.com>
2025-11-24 10:26:38 +0430

    Complete question 7 from week 3

---
+++ week-3/question-7.js
@@ -0,0 +1,19 @@
+/*
+7. Write a function to find the sum of all elements in an array 
+Description: Calculate the total sum of all array elements. 
+Example: 
+Input: 
+[1, 2, 3, 4, 5] 
+Output: 
+15
+*/
+
+function SumOfElement(numbers) {
+  var sum = 0;
+  for (let index = 0; index < numbers.length; index++) {
+    sum += numbers[index];
+  }
+  return sum;
+}
+
+module.exports = SumOfElement;
---
+++ week-3/question-7.test.js
@@ -0,0 +1,21 @@
+const SumOfElement = require("./question-7");
+
+test("Write a function to find the sum of all elements in an array ", () => {
+  expect(SumOfElement([1, 2, 3, 4, 5])).toBe(15);
+});
+
+test("Write a function to find the sum of all elements in an array ", () => {
+  expect(SumOfElement([1, 2, 3, 4, 5, -1, -2, -3, -4, -5])).toBe(0);
+});
+
+test("Write a function to find the sum of all elements in an array ", () => {
+  expect(SumOfElement([3, 7, 4, 6, 9, 1])).toBe(30);
+});
+
+test("Write a function to find the sum of all elements in an array ", () => {
+  expect(SumOfElement([6, 14, 28, 35, 49, 1, 36, 15, 22, 44])).toBe(250);
+});
+
+test("Write a function to find the sum of all elements in an array ", () => {
+  expect(SumOfElement([6, 7, 8, 9, 10])).toBe(40);
+});
