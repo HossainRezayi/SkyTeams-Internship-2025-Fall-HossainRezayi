406c8e7a91fb3d92ac65fdc2a23d1360a173baff
HossainRezayi <hossainrezayi26.@gmail.com>
2025-11-24 16:25:06 +0430

    Complete question 15 from week 3

---
+++ week-3/question-15.js
@@ -0,0 +1,65 @@
+/*
+15. Check for Armstrong number 
+Description: A number is Armstrong if the sum of its digits raised to the power of the number of 
+digits equals the number itself. 
+Example: 
+Input: 
+153 
+Output: 
+Armstrong 
+Input: 
+123 
+Output: 
+Not Armstrong 
+*/
+
+function Multiply(number, exponent) {
+  let sum = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    sum *= number;
+  }
+  return sum;
+}
+
+function Count(number) {
+  let count = 0;
+  while (number > 0) {
+    number = Math.floor(number / 10);
+    count++;
+  }
+  return count;
+}
+
+function isArmstrong(number) {
+  let instead = number;
+  let count = Count(number);
+  let digits = Digits(count);
+  let sumDigits = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    sumDigits += Multiply(digits[i], count);
+  }
+
+  // function Multiply(digits) {
+  //   let sum = 1;
+
+  //   for (let i = 0; i < count; i++) {
+  //     sum *= digits;
+  //   }
+  //   return sum;
+  // }
+
+  function Digits(count) {
+    let arrayOfDigits = [count];
+    for (let i = 0; i < count; i++) {
+      arrayOfDigits[i] = number % 10;
+      number = Math.floor(number / 10);
+    }
+    return arrayOfDigits;
+  }
+
+  return sumDigits === instead;
+}
+
+module.exports = isArmstrong;
