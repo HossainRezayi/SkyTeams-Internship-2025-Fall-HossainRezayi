8b96329cc0ed307ace02dca76eb72167449ec0b4
HossainRezayi <hossainrezayi26.@gmail.com>
2025-11-24 10:17:19 +0430

    Complete question 6 from week 3

---
+++ week-3/question-6.js
@@ -0,0 +1,20 @@
+/*
+6. Write a function to find the smallest element in an array 
+Description: Find and return the smallest number in an array. 
+Example: 
+Input: 
+[4, 7, 1, 9, 2] 
+Output: 
+1
+*/
+
+function SmallestElement(numbers) {
+  var smallest = numbers[0];
+  for (let index = 0; index < numbers.length; index++) {
+    if (smallest > numbers[index]) smallest = numbers[index];
+  }
+
+  return smallest;
+}
+
+module.exports = SmallestElement;
